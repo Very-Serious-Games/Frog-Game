6d25f9aa9bf1602d6480969b2452670f0eccfa06
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2023-06-20 14:36:53 +0200

    mas menos mas menos

--- Frog Game/Assets/MadeByProfessorOakie/SimpleSonarShader/Example/SimpleSonarShader_ExampleCollision.cs
+++ Frog Game/Assets/MadeByProfessorOakie/SimpleSonarShader/Example/SimpleSonarShader_ExampleCollision.cs
@@ -1,16 +1,12 @@
-﻿// SimpleSonarShader scripts and shaders were written by Drew Okenfuss.
-
-using System;
+﻿using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class SimpleSonarShader_ExampleCollision : MonoBehaviour
 {
     public event EventHandler<Vector3> OnSonarLogic;
 
     [SerializeField] float divisorPotencia = 2;
-
     public GameObject sonarRingPrefab; // The sonar ring prefab
 
     private GameObject activeSonarRing; // Reference to the active sonar ring
@@ -19,12 +15,12 @@ public class SimpleSonarShader_ExampleCollision : MonoBehaviour
     {
         if (activeSonarRing == null)
         {
-            StartSonarRing(point, force / 10.0f);
+            StartSonarRing(point, force);
             OnSonarLogic?.Invoke(this, point);
 
             // Start sonar ring from the specified point
             SimpleSonarShader_Parent parent = GetComponentInParent<SimpleSonarShader_Parent>();
-            if (parent) parent.StartSonarRing(point, force / 10.0f);
+            if (parent) parent.StartSonarRing(point, force);
         }
     }
 
@@ -37,18 +33,25 @@ public class SimpleSonarShader_ExampleCollision : MonoBehaviour
     private IEnumerator ExpandSonarRing(GameObject sonarRing, float force)
     {
         float radius = 0.0f;
-        float expansionSpeed = force;
+        float expansionSpeed = 20.0f; // Adjust the expansion speed here
 
         while (radius < force)
         {
             radius += expansionSpeed * Time.deltaTime;
             sonarRing.transform.localScale = new Vector3(radius, sonarRing.transform.localScale.y, radius);
+
+            if (radius >= force)
+            {
+                // Stop the expansion when the radius exceeds the force value
+                sonarRing.transform.localScale = new Vector3(force, sonarRing.transform.localScale.y, force);
+                break;
+            }
+
             yield return null;
         }
 
         Destroy(sonarRing);
         activeSonarRing = null; // Reset the active sonar ring reference
-
     }
 
     void OnCollisionEnter(Collision collision)
